8037538bf2dfd40c4dc30725fe04b4fe39cb6e56
Brian Ledbetter <brianl@nearestisland.com>
2010-10-07 10:17:12 -0600

    Parse JSON; we can get an array here rather than just a string.

--- sunspot/lib/sunspot/deferred_indexers/deferred_indexer_delete_by_id.rb
+++ sunspot/lib/sunspot/deferred_indexers/deferred_indexer_delete_by_id.rb
@@ -2,9 +2,10 @@ module Sunspot
   module DeferredIndexers
     class DeferredIndexerDeleteById
       @queue = "DeferredIndexer"
-      def self.perform(document_id)
+      def self.perform(document_ids_json)
+        document_ids = JSON.parse(document_ids_json)
         conn = RSolr.connect :url => Sunspot.config.solr.url
-        conn.delete_by_id(document_id)
+        conn.delete_by_id(document_ids)
       end
     end
   end
